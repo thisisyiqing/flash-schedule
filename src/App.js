b8fb5ea4b37e2d44ab8267cea06aff75ecb5ac1b
unknown <marina_wyq@163.com>
2020-11-23 23:59:59 +0800

    V1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import React, { Component } from 'react'
+import { Alert } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  
+  state = {
+    visible: false
+  }
+
+  handleClose = () => {
+    this.setState({visible: false})
+  };
+  
+  render() {
+    const visible = this.state;
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        {visible ? (
+          <Alert
+            message="This is an Info Alert from Ant Design"
+            description="Just a test"
+            type="info"
+            showIcon
+            closable
+            afterClose={this.handleClose}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default App;
