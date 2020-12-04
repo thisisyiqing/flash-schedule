d1c35d7c90b0c4b5fb4ec579140ee1ba46be9578
unknown <marina_wyq@163.com>
2020-12-04 10:35:31 +0800

    V5

--- src/TimeTable.js
+++ src/TimeTable.js
@@ -9,107 +9,168 @@ export default class TimeTable extends Component {
             timeZone: this.props.timeZone,
             localTime: [],
             localDay: [],
-            cardInfo: [{left: '100px', top: '300px', title: 'abc'}],
+            cardInfo: [],
         };
+        
+        var time = this.props.time
+        console.log(time)
+        time.map((classTime, index) => {
+            console.log(classTime.days)
+            if (classTime.days.indexOf('M') !== -1) {
+                this.isMonday(index)
+            }
+             console.log(this.state.cardInfo)
+            if (classTime.days.indexOf('W') !== -1) {
+                this.isWednesday(index)
+            }
+             console.log(this.state.cardInfo)
+            if (classTime.days.indexOf('F') !== -1) {
+                this.isFriday(index)
+            }
+             console.log(this.state.cardInfo)
+            if (classTime.days.indexOf('Th') !== -1) {
+                this.isThursday(index)
+            }
+             console.log(this.state.cardInfo)
+            if (classTime.days.indexOf('T') !== -1 &&  classTime.days.indexOf('T') !== classTime.days.indexOf('Th')) {
+                this.isTuesday(index)
+            }
+             console.log(this.state.cardInfo)
+            return classTime
+        })
     }
 
     static getDerivedStateFromProps(props) {
-        if(props.timeZone === 'PST'){
-            return{
+        if (props.timeZone === 'PST') {
+            return {
                 localTime: ['8:30', '9:00', '9:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00',
-                            '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'],
+                    '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'],
                 localDay: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-            };  
+            };
         }
-        if(props.timeZone === 'CST'){
-            return{
+        if (props.timeZone === 'CST') {
+            return {
                 localTime: ['0:30', '1:00', '1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00',
-                            '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00'],
+                    '5:30', '6:00', '6:30', '7:00', '7:30', '8:00', '8:30', '9:00', '9:30', '10:00'],
                 localDay: ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-            };  
+            };
         }
         return null;
     }
 
-    componentDidMount(){
-        var time = this.props.time
-        time.map((classTime, index) => {
-            var tempList = [];
-            // if(classTime.days.indexOf('M') !== -1){
-            //     this.setState({
-            //         cardInfo: this.state.cardInfo.push({left: '100px', top: '300px', title: classTime.classes[index]})
-            //     })
-            // }
-            // if(classTime.days.indexOf('W') !== -1){
-            //     this.setState({
-            //         tempList: [{left: '200px', top: '300px', title: classTime.classes[index]}],
-            //         cardInfo: this.state.cardInfo.concat(tempList)
-            //     })
-            // }
-            // if(classTime.days.indexOf('F') !== -1){
-            //     this.setState({
-            //         cardInfo: this.state.cardInfo.push({left: '300px', top: '300px', title: classTime.classes[index]})
-            //     })
-            // }
-            // if(classTime.days.indexOf('Th') !== -1){
-            //     this.setState({
-            //         cardInfo: this.state.cardInfo.push({left: '250px', top: '300px', title: classTime.classes[index]})
-            //     })
-            // } 
-            // if(classTime.days.indexOf('T') !== classTime.days.indexOf('Th')){
-            //     this.setState({
-            //         cardInfo: this.state.cardInfo.push({left: '150px', top: '300px', title: classTime.classes[index]})
-            //     })
-            // }
-            return classTime
-         })
+    isMonday(index) {
+        console.log('i am here')
+        this.setState({
+            // cardInfo: [{ left: '100px', top: '300px', title: this.props.classes[index] }]
+            cardInfo: this.state.cardInfo.concat([1])
+        })
+        console.log(this.state.cardInfo)
     }
+
+    isTuesday(index) {
+        this.setState({
+            cardInfo: this.state.cardInfo.concat([{ left: '150px', top: '300px', title: this.props.classes[index] }])
+        })
+    }
+
+    isWednesday(index){
+        this.setState({
+            cardInfo: this.state.cardInfo.concat([{ left: '200px', top: '300px', title: this.props.classes[index] }])
+        })
+    }
+
+    isThursday(index){
+        this.setState({
+            cardInfo: this.state.cardInfo.concat([{ left: '250px', top: '300px', title: this.props.classes[index] }])
+        })
+    }
+
+    isFriday(index){
+        this.setState({
+            cardInfo: this.state.cardInfo.concat([{ left: '300px', top: '300px', title: this.props.classes[index] }])
+        })
+    }
+
+    // componentDidMount() {
+    //     var time = this.props.time
+    //     console.log(time)
+    //     time.map((classTime, index) => {
+    //         console.log(classTime.days)
+    //         if (classTime.days.indexOf('M') !== -1) {
+    //             this.isMonday(index)
+    //         }
+    //          console.log(this.state.cardInfo)
+    //         if (classTime.days.indexOf('W') !== -1) {
+    //             this.isWednesday(index)
+    //         }
+    //          console.log(this.state.cardInfo)
+    //         if (classTime.days.indexOf('F') !== -1) {
+    //             this.isFriday(index)
+    //         }
+    //          console.log(this.state.cardInfo)
+    //         if (classTime.days.indexOf('Th') !== -1) {
+    //             this.isThursday(index)
+    //         }
+    //          console.log(this.state.cardInfo)
+    //         if (classTime.days.indexOf('T') !== -1 &&  classTime.days.indexOf('T') !== classTime.days.indexOf('Th')) {
+    //             this.isTuesday(index)
+    //         }
+    //          console.log(this.state.cardInfo)
+    //         return classTime
+    //     })
+    // }
 
     render() {
         // var classes = this.props.classes
-        // var classCard = this.state.cardInfo.map((card) => {
-        //     return(
-        //         <Card title={card.title} bordered={false} style={{position: 'absolute', marginLeft: card.left, marginTop: card.top, width: '50px'}}>
-        //             <p>card.title</p>
-        //         </Card>
-        //     )
-        // });
+
+        var classCard = this.state.cardInfo.map((card, index) => {
+            const newKey = index + 50
+            return (
+                <Card key={newKey} title={card.title} bordered={false} style={{ position: 'absolute', marginLeft: card.left, marginTop: card.top, width: '50px' }}>
+                    <p>card.title</p>
+                </Card>
+            )
+        });
 
         var gridTitle = this.state.localDay.map((day, index) => {
             const newKey = index + 100
             return (
                 <th key={newKey} className="text-uppercase">{day}</th>
             )
-          }
+        }
         );
 
         var grid = this.state.localTime.map((localTime, index) => {
             return (
                 <tr key={index}>
                     <td className="align-middle">{localTime}</td>
-                    <td className="bg-light-gray" style={{backgroundColor: this.state.MondayColor}}></td>
-                    <td className="bg-light-gray" style={{backgroundColor: this.state.TuesdayColor}}></td>
-                    <td className="bg-light-gray" style={{backgroundColor: this.state.WednesdayColor}}></td>
-                    <td className="bg-light-gray" style={{backgroundColor: this.state.ThursdayColor}}></td>
-                    <td className="bg-light-gray" style={{backgroundColor: this.state.FridayColor}}></td>
+                    <td className="bg-light-gray" style={{ backgroundColor: this.state.MondayColor }}></td>
+                    <td className="bg-light-gray" style={{ backgroundColor: this.state.TuesdayColor }}></td>
+                    <td className="bg-light-gray" style={{ backgroundColor: this.state.WednesdayColor }}></td>
+                    <td className="bg-light-gray" style={{ backgroundColor: this.state.ThursdayColor }}></td>
+                    <td className="bg-light-gray" style={{ backgroundColor: this.state.FridayColor }}></td>
                 </tr>
             )
-          }
+        }
         );
 
         return (
             <div className="container">
                 <table className="table table-bordered text-center">
-                    <tr className="bg-light-gray"> 
-                        <th className="align-middle">Schedule {this.props.index}</th>
-                        {gridTitle}
-                    </tr>
-                    {grid}
+                    <thead>
+                        <tr className="bg-light-gray">
+                            <th className="align-middle">Schedule {this.props.index}</th>
+                            {gridTitle}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {grid}
+                    </tbody>
                 </table>
-                {/* {classCard} */}
+                {classCard}
             </div>
         )
-        
+
         // return (
         //     <div className="container">
         //         <div className="table-responsive">
